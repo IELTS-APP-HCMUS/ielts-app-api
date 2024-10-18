85494e68f5fa70df6e0debfe90541158a1a57f04
Mai Nhật Nam <22120219@student.hcmus.edu.vn>
2024-10-18 12:41:34 +0700

    Add refractor code - field name

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,10 +1,15 @@
 package models
 
-import "time"
+import (
+	"ielts-app-api/common"
+	"time"
+)
 
 type User struct {
-	ID          string `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Email       string `gorm:"uniqueIndex"`
+	ID          string  `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Email       string  `gorm:"uniqueIndex"`
+	FirstName   *string `gorm:"column:first_name"`
+	LastName    *string `gorm:"column:last_name"`
 	Password    string
 	Role        string
 	DateCreated time.Time `gorm:"column:date_created;autoCreateTime"`
@@ -12,5 +17,5 @@ type User struct {
 
 func (User) TableName() string {
 	// define common variable here
-	return "public.users"
+	return common.POSTGRES_TABLE_NAME_USERS
 }
