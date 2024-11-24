1e95f61858cc373464a9e969949d621ef11cdd9e
Mai Nhật Nam <22120219@student.hcmus.edu.vn>
2024-11-24 18:51:20 +0700

    WINP-70 models defined. Other tasks must cherry-pick

--- common/common.go
+++ common/common.go
@@ -2,9 +2,17 @@ package common
 
 // Define New Table Name here
 const (
-	POSTGRES_TABLE_NAME_USERS   = "public.users"
-	POSTGRES_TABLE_NAME_ROLES   = "public.roles"
-	POSTGRES_TABLE_NAME_TARGETS = "public.student_target"
+	POSTGRES_TABLE_NAME_USERS           = "public.users"
+	POSTGRES_TABLE_NAME_ROLES           = "public.roles"
+	POSTGRES_TABLE_NAME_TARGETS         = "public.student_target"
+	POSTGRES_TABLE_NAME_QUIZ            = "public.quiz"
+	POSTGRES_TABLE_NAME_PART            = "public.part"
+	POSTGRES_TABLE_NAME_QUESTION        = "public.question"
+	POSTGRES_TABLE_NAME_TAG_SEARCH      = "public.tag_search"
+	POSTGRES_TABLE_NAME_TAG_POSITION    = "public.tag_search_position"
+	POSTGRES_TABLE_NAME_QUIZ_TAG_SEARCH = "public.quiz_tag_search"
+	POSTGRES_TABLE_NAME_QUIZ_SKILL      = "public.type"
+	POSTGRES_TABLE_NAME_QUIZ_PART       = "public.quiz_part"
 )
 
 // Define New Const variable here for each service
@@ -16,3 +24,9 @@ const (
 )
 
 // Define other common variable here
+const (
+	QUESTION_TYPE_SINGLE_RADIO      = "SINGLE-RADIO"
+	QUESTION_TYPE_SINGLE_SELECTION  = "SINGLE-SELECTION"
+	QUESTION_TYPE_FILL_IN_THE_BLANK = "FILL-IN-THE-BLANK"
+	QUESTION_TYPE_MULTIPLE          = "MULTIPLE"
+)
