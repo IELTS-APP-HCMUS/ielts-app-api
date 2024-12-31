89641cb1d8dc8c77a24581b267145f30898a7ea1
Mai Nhật Nam <22120219@student.hcmus.edu.vn>
2024-12-31 11:26:27 +0700

    Fix API Vocab Search Vocab with wrong index

--- internal/services/vocab.go
+++ internal/services/vocab.go
@@ -77,26 +77,38 @@ func (s *Service) DeleteVocab(ctx context.Context, userId string, vocabValue mod
 func (s *Service) GetReadingVocab(ctx context.Context, request models.LookUpVocabRequest) (*models.Vocab, error) {
 	vocabId := fmt.Sprintf("%d_%d_%d", request.QuizId, request.SentenceIndex, request.WordIndex)
 
+	// Tìm vocab trong câu cụ thể trước
 	vocab, err := s.vocabRepo.GetDetailByConditions(ctx, func(tx *gorm.DB) {
 		tx.Where("vocab_id = ?", vocabId)
 	})
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
+			// Nếu không tìm thấy trong câu, thử tìm kiếm theo câu
 			vocab, err = s.LookUpVocabLinear(ctx, request.QuizId, request.SentenceIndex, request.Word)
 			if err != nil {
-				return nil, err
+				if errors.Is(err, gorm.ErrRecordNotFound) {
+					// Nếu vẫn không tìm thấy, tìm trên toàn bài
+					vocab, err = s.LookUpVocabGlobal(ctx, request.QuizId, request.Word)
+					if err != nil {
+						return nil, err
+					}
+				} else {
+					return nil, err
+				}
 			}
 		} else {
 			return nil, err
 		}
 	} else {
+		// Nếu tìm thấy nhưng giá trị không khớp, tìm lại trên toàn bài
 		if vocab.Value != request.Word {
-			vocab, err = s.LookUpVocabLinear(ctx, request.QuizId, request.SentenceIndex, request.Word)
+			vocab, err = s.LookUpVocabGlobal(ctx, request.QuizId, request.Word)
 			if err != nil {
 				return nil, err
 			}
 		}
 	}
+
 	vocab.Explanation = strings.ReplaceAll(vocab.Explanation, "\"", "")
 	return vocab, nil
 }
@@ -105,10 +117,33 @@ func (s *Service) LookUpVocabLinear(ctx context.Context, quizId int, sentenceInd
 	vocabIdPattern := fmt.Sprintf("%d_%d_%%", quizId, sentenceIndex)
 
 	vocab, err := s.vocabRepo.GetDetailByConditions(ctx, func(tx *gorm.DB) {
-		tx.Where("vocab_id LIKE ? AND value = ?", vocabIdPattern, word)
+		tx.Where("vocab_id LIKE ? AND value ILIKE ?", vocabIdPattern, word)
 	})
 	if err != nil {
 		return nil, err
+	}
+
+	return vocab, nil
+}
+
+func (s *Service) LookUpVocabGlobal(ctx context.Context, quizId int, word string) (*models.Vocab, error) {
+	vocabIdPattern := fmt.Sprintf("%d_%%", quizId)
+
+	vocab, err := s.vocabRepo.GetDetailByConditions(ctx, func(tx *gorm.DB) {
+		tx.Where("vocab_id LIKE ? AND value ILIKE ?", vocabIdPattern, word)
+	})
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			// Nếu không tìm thấy từ, lấy đại một từ nào đó trong bài
+			vocab, err = s.vocabRepo.GetDetailByConditions(ctx, func(tx *gorm.DB) {
+				tx.Where("vocab_id LIKE ?", vocabIdPattern).Limit(1)
+			})
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
 	}
 
 	return vocab, nil
